6d3eae92ae9a89590132f8458fc1740cb1365c64
yasainet <takumi.mizoguchi@gmail.com>
2014-04-15 07:07:21 -0700

    add /home/yasainet

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ for dotfile in .vim .vimrc .zshrc
 do
   rm -fr ~/$dotfile
   #ln -vnfs $PWD/$dotfile ~/$dotfile
-  ln -vnfs /home/yasainet/dotfiles/$dotfile /home/yasainet/$dotfile
+  ln -vnfs $HOME/dotfiles/$dotfile $HOME/$dotfile
 done

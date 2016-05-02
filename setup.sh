dd2a74dde4059098761710835402b1283a2868a4
yasainet <takumi.mizoguchi@gmail.com>
2016-05-02 11:24:23 +0900

    add setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for dotfile in .vim .vimrc .zshrc
 do
+  cp -rp .oh-my-zsh-customs/theme/zsh/* oh-my-zsh/themes
   rm -fr ~/$dotfile
   ln -vnfs $HOME/dotfiles/$dotfile $HOME/$dotfile
 done
